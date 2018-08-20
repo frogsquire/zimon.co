544ca4dba8e548d4a0283ca141a6fb402485a612
max <max@zimon.co>
2018-08-19 21:29:41 -0500

    #1: update to jquery 3.3.1; use JS to show all sections at once; remove separate all.html

--- js/page.js
+++ js/page.js
@@ -1,20 +1,41 @@
-	$(document).ready(function() {
+$(document).ready(function() {
+		var allShown = false; 
+		hideSections(); 
+		load($(".header-name > a"), null, "#welcome");
 
-		$(document).on("click", "header > ul > li > a", function(event){
-			event.preventDefault();
+		$(document).on("click", "header > ul > li > a", function(e){
+			e.preventDefault();
+			if(allShown) {
+				hideSections(); 
+				allShown = false; 
+			}
 			var element = this;  
 			var active_tab_selector = $('header > ul > li.active > a').attr('href');	
 			var target_tab_selector = $(this).attr('href');
 			load(element, active_tab_selector, target_tab_selector); 
 	     });
 
+		$(document).on("click", "#all", function(e) {
+			e.preventDefault(); 
+			$(".content-section").each(function() {
+				$(this).show(); 
+			}); 
+			allShown = true; 
+		});
+
 		function load(element, active, toLoad) {
 			var actived_nav = $('header > ul > li.active');
 			actived_nav.removeClass('active');
 			$(element).parents('li').addClass('active');
-			$(active).removeClass('active');
-			$(active).addClass('hide');
-			$(toLoad).removeClass('hide');
-			$(toLoad).addClass('active');
+			if(active) { 
+				$(active).hide(); 
+			}
+			$(toLoad).show(); 
 		}
-	  });
+});
+
+var hideSections = function() {
+	$(".content-section").each(function() {
+			$(this).hide(); 
+	}); 
+}
