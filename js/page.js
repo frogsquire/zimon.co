d842b6b9e612856e7e387639903791ef34c8b9cf
Maxwell Zimon <max@zimon.co>
2018-08-19 23:20:45 -0400

    #2: add rewrite rule /url -> #url; load section corresponding to #url; js improvements

--- js/page.js
+++ js/page.js
@@ -1,41 +1,21 @@
-$(document).ready(function() {
-		var allShown = false; 
-		hideSections(); 
-		load($(".header-name > a"), null, "#welcome");
+$(document).ready(function() { 
+		var toLoad = window.location.hash; 
+		if(!toLoad || $(toLoad).length<=0) {
+			toLoad = "#welcome";
+		}
+		$(toLoad).show(); 
 
-		$(document).on("click", "header > ul > li > a", function(e){
+		$(document).on("click", ".header-link", function(e){
 			e.preventDefault();
-			if(allShown) {
-				hideSections(); 
-				allShown = false; 
-			}
-			var element = this;  
-			var active_tab_selector = $('header > ul > li.active > a').attr('href');	
-			var target_tab_selector = $(this).attr('href');
-			load(element, active_tab_selector, target_tab_selector); 
-	     });
+			$(".content-section:visible").hide();
+			var location = $(this).attr('href'); 
+			$(location).show(); 
+		});
 
 		$(document).on("click", "#all", function(e) {
 			e.preventDefault(); 
 			$(".content-section").each(function() {
 				$(this).show(); 
 			}); 
-			allShown = true; 
 		});
-
-		function load(element, active, toLoad) {
-			var actived_nav = $('header > ul > li.active');
-			actived_nav.removeClass('active');
-			$(element).parents('li').addClass('active');
-			if(active) { 
-				$(active).hide(); 
-			}
-			$(toLoad).show(); 
-		}
 });
-
-var hideSections = function() {
-	$(".content-section").each(function() {
-			$(this).hide(); 
-	}); 
-}
